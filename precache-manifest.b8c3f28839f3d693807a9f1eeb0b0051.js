101933db90426ee0a03744596ff18f0dbba92095
Rutger Engel <rutgerengelfreelance@gmail.com>
2019-08-02 16:23:45 +0200

    Updates

---
+++ precache-manifest.b8c3f28839f3d693807a9f1eeb0b0051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3b185d11250f8b45bc4de299bdc335",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a28a10d88e9ab8dfcdd1",
+    "url": "/monsters-rolodex/static/css/main.f80cf294.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a28a10d88e9ab8dfcdd1",
+    "url": "/monsters-rolodex/static/js/main.4865cd65.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
